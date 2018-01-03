30a77aa9543ad1cf21426653fef674b38a1bcd2e
Peter Wang <happypeter1983@gmail.com>
2018-01-03 14:47:33 +0800

    add react-router

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import HomeContainer from '../containers/HomeContainer'
+import PostContainer from '../containers/PostContainer'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Route exact path='/' component={HomeContainer} />
+            <Route path='/post' component={PostContainer} />
+          </div>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default Main
---
+++ src/containers/PostContainer.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Post from '../components/Post'
+
+const PostContainer = props => <Post {...props} />
+
+export default PostContainer
