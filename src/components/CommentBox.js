cac3d2306419c29384325011ca9e96519bfc225b
Peter Wang <happypeter1983@gmail.com>
2018-01-03 17:04:37 +0800

    form style

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -41,13 +41,13 @@ class CommentBox extends Component {
   
   render () {
     const cmtForm = (
-      <div>
-        <input 
+      <FormWrap>
+        <Input 
           value={this.state.text} 
           onChange={this.handleChange}
         />
-        <button onClick={this.submitCmt}>提交</button>
-      </div>
+        <Button onClick={this.submitCmt}>提交</Button>
+      </FormWrap>
     )
 
     const { comments } = this.state
@@ -59,7 +59,9 @@ class CommentBox extends Component {
     return (
       <Wrap>
         {cmtForm}
-        {cmtList}
+        <CmtList>
+          {cmtList}
+        </CmtList>
       </Wrap>
     )
   }
@@ -72,5 +74,42 @@ const Wrap = styled.div`
   width: 400px;
   margin: 30px auto;
   box-shadow: 0 2px 2px rgba(0, 0, 0, .5);
-  height: 300px;
+  min-height: 300px;
+`
+const FormWrap = styled.div`
+  display: flex;
+  padding: 10px;
+`
+const Input = styled.input`
+  flex-grow: 1;
+  height: 30px;
+  line-height: 30px;
+  border: 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  :focus {
+    outline: none;
+    border-bottom: 1px solid deeppink;
+  }
+`
+
+const Button = styled.div`
+  background-color: deeppink;
+  margin-left: 10px;
+  padding: 0 10px;
+  border: none;
+  line-height: 30px;
+  :focus {
+    outline: none;
+  }
+  :hover {
+    cursor: pointer;
+  }
+  color: white;
+`
+
+const CmtList = styled.div`
+  padding: 10px;
+  div {
+    line-height: 30px;
+  }
 `
