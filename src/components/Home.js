c1171cf1539743f2dc9a5d92535e3dabb5223ecc
Peter Wang <happypeter1983@gmail.com>
2018-01-04 15:28:21 +0800

    post list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   render () {
+    const { posts } = this.props
+    const postList = posts.map(
+      post => (
+        <Link key={post.id} to={`/post/${post.id}`}>
+          {post.title}
+        </Link>
+      )
+    )
     return (
       <div>
-        Home
+        {postList}
       </div>
     )
   }
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Home from '../components/Home'
+import { connect } from 'react-redux'
 
 const HomeContainer = props => <Home {...props} />
 
-export default HomeContainer
+const mapStateToProps = state => ({
+  posts: state.posts
+})
+
+export default connect(mapStateToProps)(HomeContainer)
